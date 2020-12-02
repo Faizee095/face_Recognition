efb12e812a0875ee0d3590bc3f80c4ba1a42ec52
faizee <faiyazsiddique9@gmail.com>
2020-12-02 19:50:12 +0530

    Correcting data set

--- detector.py
+++ detector.py
@@ -2,13 +2,13 @@ import cv2
 import numpy as np
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.load('trainner/trainner.yml')
+recognizer.read('trainner/trainner.yml')
 cascadePath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath)
 
 
 cam = cv2.VideoCapture(0)
-font = cv2.cv.InitFont(cv2.cv.CV_FONT_HERSHEY_SIMPLEX, 1, 1, 0, 1, 1)
+font = cv2.FONT_HERSHEY_SIMPLEX
 while True:
     ret, im =cam.read()
     gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
@@ -21,7 +21,7 @@ while True:
                 Id="FAIZEE"
         else:
             Id="Unknown"
-        cv2.cv.PutText(cv2.cv.fromarray(im),str(Id), (x,y+h),font, 255)
+        cv2.putText(im,str(Id), (x,y+h),font,2,(255,255,255) )
     cv2.imshow('im',im) 
     if cv2.waitKey(10) & 0xFF==ord('q'):
         break
